c007d03fe5c5ac6f29734ea90104e8321a5f3fb6
Todd0554 <toddy9229@outlook.com>
2023-02-20 21:15:51 +1100

    add google map for contacts

---
+++ src/components/GoogleMapSite.js
@@ -0,0 +1,44 @@
+import React, {useEffect} from 'react'
+import { Loader } from "@googlemaps/js-api-loader";
+import {Container} from "react-bootstrap";
+
+const GoogleMapSite = () => {
+    
+  // use the googlemap apiKey and set the places library
+    const location = {
+        lat: -37.78814220376919,
+        lng: 145.15254641554145
+    };
+    const loader = new Loader({
+        apiKey: process.env.REACT_APP_GOOGLE_MAP_API_KEY,
+        libraries: ["places"]
+    });
+  // set the lng and lat which will be passed from props for a place, and then the zoom is 15 
+    const mapOptions = {
+        center: location,
+        zoom: 18
+    };
+
+      useEffect(() => {
+        loader
+        .load()
+        .then((google) => {
+            const map = new google.maps.Map(document.getElementById("map"), mapOptions);
+            new google.maps.Marker({
+                position: location,
+                map: map,
+                title: 'Hello World!'
+              });
+        })
+        .catch(e => {
+          console.log(e.message)
+        });
+        // eslint-disable-next-line 
+      }, [mapOptions])
+  return (    
+    <Container id="map" style={{ margin:"auto 0", height:"60vh"}}></Container>
+    
+  )
+}
+
+export default GoogleMapSite
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import GoogleMapSite from '../components/GoogleMapSite'
 
 function Contacts() {
   return (
-    <div>Contacts</div>
+    <div>
+      <h1>Contacts</h1>
+      <p>
+        We meet at the Doncaster RSL Hall, cnr Doncaster Rd and Leeds St, Doncaster East just east of the Jackson Crt Shopping Centre. The Hall has parking at the rear.
+      </p>
+      <p>
+        When the 2nd Wednesday of each month from February to December at 8pm.
+      </p>
+      <GoogleMapSite />
+    </div>
   )
 }
 
