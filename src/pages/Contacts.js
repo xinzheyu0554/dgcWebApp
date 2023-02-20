0c33b8f5272c04b38897ae994c8e2974877debf8
Todd0554 <toddy9229@outlook.com>
2023-02-20 21:18:50 +1100

    add google map for contacts

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -5,6 +5,7 @@ function Contacts() {
   return (
     <div>
       <h1>Contacts</h1>
+      
       <p>
         We meet at the Doncaster RSL Hall, cnr Doncaster Rd and Leeds St, Doncaster East just east of the Jackson Crt Shopping Centre. The Hall has parking at the rear.
       </p>
