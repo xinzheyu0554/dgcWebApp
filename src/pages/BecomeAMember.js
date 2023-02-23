3f8eab10c3a0d21c57041e7e6e609794f696394e
Todd0554 <toddy9229@outlook.com>
2023-02-24 08:48:16 +1100

    application form & 2022 newsletters

--- src/pages/BecomeAMember.js
+++ src/pages/BecomeAMember.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function BecomeAMember() {
   return (
-    <div>BecomeAMember</div>
+    <div>
+        <h1>How to become a member?</h1>
+        <p>Please follow the instructions and finish the application form.</p>
+        <iframe title="member" src="/documentation/Doncaster Garden Club Membership application in word.pdf" width="100%" height="500px"></iframe>
+    </div>
   )
 }
 
--- src/pages/Newsletters.js
+++ src/pages/Newsletters.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 
 function Newsletters() {
 
-  const Newsletters = [
+  const Newsletters2022 = [
+    "Garden club newsletter February 2022 pub.pdf",
+    "Garden club newsletter March 2022 pub.pdf",
+    "Garden club newsletter april 2022 pub.pdf",
+    "Garden club newsletter May 2022 pub.pdf",
+    "Garden club newsletter jUNE 2022 pub.pdf",
+    "Garden club newsletter July 2022 pub.pdf",
+    "Garden club newsletter August 2022.pdf",
+    "Garden club newsletter September  2022.pdf",
+    "Garden club newsletter October 2022.pdf",
+    "Garden club newsletter November 2022.pdf",
+    "Garden club newsletter NewsLetter Dec 2022.pdf",
+  ]
+
+  const Newsletters2021 = [
     "Garden club newsletter February 2021 -.pdf",
     "Garden club newsletter March 2021.pdf",
     "Garden club newsletter April 2021.pdf",
@@ -20,16 +35,35 @@ function Newsletters() {
   return (
     <div>
       <h1>Newsletters</h1> 
-
-      {Newsletters.map((a) => {
-        return (
-          <>
-            <a target="_blank" href={"/documentation/newsletter/" + a}>{a}</a>
-            <br/>
-          </>
-
-        )
-      })}
+      <Row style={{ textAlign:"center"}}>
+      <Col lg={6}>
+        <h2>2022</h2>
+        {Newsletters2022.map((a) => {
+          return (
+            <>
+              <a target="_blank" rel="noreferrer" href={"/documentation/newsletter/2022/" + a}>{a}</a>
+              <br/>
+            </>
+          )
+        })}
+      </Col>
+      
+      
+      <br/>
+      <Col lg={6}>
+        <h2>2021</h2>
+        {Newsletters2021.map((a) => {
+          return (
+            <>
+              <a target="_blank" rel="noreferrer" href={"/documentation/newsletter/2021/" + a}>{a}</a>
+              <br/>
+            </>
+          )
+        })}
+      </Col>
+      </Row>
+      
+      
       
     </div>
   )
