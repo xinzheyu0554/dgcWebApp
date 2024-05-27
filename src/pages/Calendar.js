238b88f8764b108f1f7f96efaded2d26e7b5773c
Todd0554 <toddy9229@outlook.com>
2024-05-27 11:31:55 +1000

    uploading notification in calendar

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -5,10 +5,9 @@ function Calendar() {
     <div style={{margin:"10px"}}>
       <div style={{padding:"10px"}}>
               <h1>Calendar</h1>
-      <h2>Welcome to 2023!</h2>
+      <h2>Welcome to 2024!</h2>
       <iframe title="2023" src="/documentation/Calander of Events 2024.pdf" width="100%" height="800px"></iframe>
       </div>
-
     </div>
   )
 }
